59bdaf9fa8849edb885b867e377e626af2d151ec
Erich Heine <sophacles@gmail.com>
2019-03-24 15:38:12 -0500

    More honing

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,15 @@ use rand::Rng;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::timer::{Delay, Interval};
 
+const TRAP_TIME: u64 = 10;
+const STATS_INTERVAL: u64 = 15;
+
 static CON: AtomicUsize = AtomicUsize::new(0);
 static DIS: AtomicUsize = AtomicUsize::new(0);
 
 fn main() {
     let addr = "0.0.0.0:22".parse::<SocketAddr>().unwrap();
-    let listener = TcpListener::bind(&addr).unwrap();
-    let sock_stream = listener.incoming().map_err(|_| ());
+    let sock_stream = TcpListener::bind(&addr).unwrap().incoming().map_err(|_| ());
 
     tokio::run(lazy(move || {
         stats_outputter();
@@ -32,13 +34,9 @@ fn main() {
 
 fn stats_outputter() {
     tokio::spawn(
-        Interval::new_interval(Duration::from_secs(15))
+        Interval::new_interval(Duration::from_secs(STATS_INTERVAL))
             .map_err(|_| println!("Stats timer failed!"))
-            .for_each(move |_| {
-                let c = CON.load(Ordering::Relaxed);
-                let d = DIS.load(Ordering::Relaxed);
-                Ok(println!("\nStats: {} total, {} current\n", c, c - d))
-            }),
+            .for_each(|_| Ok(notify_stats())),
     );
 }
 
@@ -58,17 +56,11 @@ fn tarpit(client: TcpStream) {
 
 fn slow_data(client: TcpStream) -> impl Future<Item = TcpStream, Error = SocketAddr> {
     let raddr = client.peer_addr().unwrap();
-    Delay::new(Instant::now() + Duration::from_secs(10))
+    Delay::new(Instant::now() + Duration::from_secs(TRAP_TIME))
         .map_err(|_| std::io::ErrorKind::Other.into())
         .and_then(|_| tokio::io::write_all(client, random_text()))
         .map(|(client, _)| client)
         .map_err(move |_| raddr)
-}
-
-fn random_text() -> String {
-    let mut rng = rand::thread_rng();
-    let r: u64 = rng.gen();
-    format!("{:x}\r\n", r)
 }
 
 fn notify_conn(client: &TcpStream) {
@@ -81,4 +73,16 @@ fn notify_disconn(raddr: SocketAddr, timer: Instant) {
     DIS.fetch_add(1, Ordering::Relaxed);
     let stop: Duration = Instant::now().duration_since(timer);
     println!("Connection broken: {} (lasted: {}s)", raddr, stop.as_secs());
+}
+
+fn notify_stats() {
+    let c = CON.load(Ordering::Relaxed);
+    let d = DIS.load(Ordering::Relaxed);
+    println!("\nStats: {} total, {} current\n", c, c - d)
+}
+
+fn random_text() -> String {
+    let mut rng = rand::thread_rng();
+    let r: u64 = rng.gen();
+    format!("{:x}\r\n", r)
 }
